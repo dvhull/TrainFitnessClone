1910edcd2b125ef0538421d7a62a1e28ed4fdb66
dvhull <56657018+dvhull@users.noreply.github.com>
2025-01-23 13:12:56 -0500

    Initial Commit

---
+++ TrainFitnessDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrainFitnessDemo
+//
+//  Created by David Hull on 2025-01-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrainFitnessDemo/TrainFitnessDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainFitnessDemoApp.swift
+//  TrainFitnessDemo
+//
+//  Created by David Hull on 2025-01-23.
+//
+
+import SwiftUI
+
+@main
+struct TrainFitnessDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
